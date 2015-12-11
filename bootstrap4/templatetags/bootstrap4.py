812beaff9f20e42c1deee3d6783c82027657032c
Sebastian Braun <sebastian@elmnt.de>
2015-12-11 12:50:12 +0100

    bugfix

--- bootstrap4/templatetags/bootstrap4.py
+++ bootstrap4/templatetags/bootstrap4.py
@@ -3,6 +3,19 @@
 
 from __future__ import unicode_literals
 
+from django.template import Library
+from django.template import Node
+
+
+register = Library()
+
+
+@register.assignment_tag(takes_context=True)
+def get_bootstrap_anchors(context, placeholder="content", plugin="SectionPlugin"):
+    if 'current_page' not in context:
+        return []
+    return context['current_page'].placeholders.get(slot=placeholder).get_plugins().filter(plugin_type=plugin).order_by('position').values_list('section__slug', 'section__name')
+
 
 # TODO:
 # LOAD A LIST FROM A PAGE WHICH CONTAINS ALL
@@ -10,4 +23,3 @@ from __future__ import unicode_literals
 # USED TO GENERATE A NAVIGATIONBAR
 # WITH INTERNAL CONTENT
 
-# page.placeholders.get(slot='content').get_plugins().filter(plugin_type='SectionPlugin').values_list('section__name', 'section__slug')
