75d234bdc03c0e2b48321abe1922f1a2b0de11eb
Sebastian Braun <sebastian@elmnt.de>
2015-12-11 11:59:38 +0100

    added templatetag (empty) with queryset informations

---
+++ bootstrap4/templatetags/bootstrap4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# ex:set fileencoding=utf-8:
+
+from __future__ import unicode_literals
+
+
+# TODO:
+# LOAD A LIST FROM A PAGE WHICH CONTAINS ALL
+# Sections WITH THEIR name AND slug ATTRIBUTES
+# USED TO GENERATE A NAVIGATIONBAR
+# WITH INTERNAL CONTENT
+
+# page.placeholders.get(slot='content').get_plugins().filter(plugin_type='SectionPlugin').values_list('section__name', 'section__slug')
