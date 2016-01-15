56aa8ab52a5feed1da8a9b62eb86cb6382fb1070
Sebastian Braun <sebastian@elmnt.de>
2016-01-15 04:39:33 +0100

    update easy_thumbnails

--- homepage/settings_default.py
+++ homepage/settings_default.py
@@ -170,11 +170,15 @@ FILER_STORAGES = {
 #TEXT_SAVE_IMAGE_FUNCTION='cmsplugin_filer_image.integrations.ckeditor.create_image_plugin'
 
 THUMBNAIL_PROCESSORS = (
+
     'easy_thumbnails.processors.colorspace',
     'easy_thumbnails.processors.autocrop',
+    # 'easy_thumbnails.processors.scale_and_crop',
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters',
+    'easy_thumbnails.processors.background',
 )
+
 THUMBNAIL_QUALITY = 90
 
 
